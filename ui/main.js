a5b7c192be09629267db5182a57ef32d2f0e08fa
AUnni <30656531+AUnni@users.noreply.github.com>
2017-09-23 18:35:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,11 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
+
+
+var img = document.getElementById('madi');
+img.onclick = function () {
+    imf.style.marginLeft = '100px';
+    
+};
+
