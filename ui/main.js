5e477fc92636bf589cdb51f319c770bd31258ca9
AUnni <30656531+AUnni@users.noreply.github.com>
2017-09-23 20:34:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,10 +18,10 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick = function() {
-    var names = ['name1','name 2','name 3'];
+    var names = ['name1','name 2','name 3','name 4'];
     var list = '';
     for (var i=0; i< names.length; i++) {
-        list += '<li>'+ names[i] '</li>';
+        list += '<li>'+ names[i] + '</li>';
         
 }
 var ul = document.getElementById('namelist');
