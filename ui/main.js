4e62286a02d9ec14a64a7a53d49c889e41a64282
AUnni <30656531+AUnni@users.noreply.github.com>
2017-09-23 21:13:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,8 +14,6 @@ button.onclick = function(){
     request.open('GET', "http://achuthanunni.imad.hasura-app.io/counter", true);
     request.send(null);
 };
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick = function() {
     var request = new XMLHttpRequest(); 
@@ -36,6 +34,9 @@ ul.innerHTML = list;
         }
         }
         };
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+
     request.open('GET', "http://achuthanunni.imad.hasura-app.io/submit-name?name=" + name, true);
     request.send(null);
     
